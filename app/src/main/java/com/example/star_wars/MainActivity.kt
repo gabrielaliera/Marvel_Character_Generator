703336132c5115005c527198254d4788a8e6db3f
gabrielaliera <gliera2990@gmail.com>
2023-03-29 12:28:06 -0700

    add keys

--- app/src/main/java/com/example/star_wars/MainActivity.kt
+++ app/src/main/java/com/example/star_wars/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.star_wars
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
 
 class MainActivity : AppCompatActivity() {
+
+    var characterImageURL = ""
+    val timestamp = System.currentTimeMillis()
+    val privateKey = ""
+    val publicKey = ""
+    val hash = stringToMd5("$timestamp$privateKey$publicKey")
+
+    val url =
+        "https://gateway.marvel.com/v1/public/characters?ts=$timestamp&apikey=$publicKey&hash=$hash"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun characterImageURL( url : String){
+        val client = AsyncHttpClient()
+        client[url, object : JsonHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Headers,
+                json: JsonHttpResponseHandler.JSON
+            ) {
+                Log.d("Marvel", "response successful$json")
+                characterImageURL = json.jsonObject.getString("message")
+                Log.d("MarvelImageURL", "Marvel image URL set")
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                errorResponse: String,
+                throwable: Throwable?
+            ) {
+                Log.d("Marvel Error", errorResponse)
+            }
+        }]
     }
 }
